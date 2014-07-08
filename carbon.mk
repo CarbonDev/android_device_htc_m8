84fda3832f88cc1b2994aa77b77ad099d218a146
Simon Sickle <simon@simonsickle.com>
2014-07-07 22:00:30 -0700

    m8: fix typo in carbon.mk preventing lunch
    Change-Id: I013a86cfaca9e6444b5ec82ff0c5ce8b41b51489

--- carbon.mk
+++ carbon.mk
@@ -4,6 +4,6 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 #$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_NAME := carbon_m8
